b93ebff4fa49679e75714aa11e50322e9e18e2db
dark1ing0 <karev.rk@gmail.com>
2025-10-11 10:34:23 +0300

    Add files via upload

---
+++ parse_example/data_parser.py
@@ -0,0 +1,73 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import json
+import os  # чтобы работать с путями
+
+URL = "https://ru.citaty.net/"  # URL
+
+# Определяем директорию скрипта
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parquet_path = os.path.join(current_dir, "quotes.parquet")
+
+try:
+    response = requests.get(
+        URL, headers={"User-Agent": "Mozilla/5.0"}, timeout=10, allow_redirects=True
+    )
+
+    # Проверяем доступность сайта
+    if response.status_code != 200:
+        print(f"Ошибка: сайт недоступен ({response.status_code})")
+        exit()
+    response.raise_for_status()
+except requests.RequestException as e:
+    print("Ошибка при запросе:", e)
+    raise SystemExit(1)
+
+soup = BeautifulSoup(
+    response.text, "html.parser"
+)  # Создаём объект BeautifulSoup для разбора HTML
+
+# Сбор данных с защитой от дублей
+seen = set()  # здесь будем хранить уникальные ключи (quote, author)
+products = []
+
+for item in soup.find_all("div", class_="blockquote"):
+    quote_elem = item.find("p", class_="blockquote-text")
+    quote = (
+        quote_elem.get_text(strip=True).replace("\xa0", " ") if quote_elem else "N/A"
+    )
+    author_elem = item.find("a", class_="link")
+    author = author_elem.get_text(strip=True) if author_elem else "N/A"
+    if quote != "N/A" and len(quote) > 10:
+        products.append({"quote": quote, "author": author})
+
+        # Приведение типов
+        products.append({"quote": str(quote), "author": str(author)})
+
+        # Проверяем, что данные найдены
+if not products:
+    print("Цитаты не найдены. Проверьте структуру сайта.")
+    exit()
+
+    # Преобразуем список словарей в DataFrame
+df = pd.DataFrame(products)
+
+# (опционально) на всякий случай можно ещё раз удалить дубликаты в DataFrame
+df = df.drop_duplicates(subset=["quote", "author"]).reset_index(drop=True)
+
+# Настройки pandas для полного отображения цитат
+pd.set_option("display.max_colwidth", None)
+# Красивый вывод первых 10 цитат
+print("Парсинг завершён успешно!")
+print(f"Найдено {len(df)} цитат.\n")
+print("Первые 10 цитат:\n")
+for i, row in df.head(10).iterrows():
+    print(f"{i+1}. {row['quote']} — {row['author']}\n")
+
+print("Типы колонок:")
+print(df.dtypes)
+
+# Сохраняем в parquet в той же папке
+df.to_parquet(parquet_path, index=False)
+print(f"Паркет сохранён в: {parquet_path}")
