a344554af9a963485f01e31d5a2e945c777e16a3
dark1ing0 <karev.rk@gmail.com>
2025-10-19 13:05:25 +0300

    Add files via upload

--- write_to_db.py
+++ write_to_db.py
@@ -19,6 +19,27 @@ file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 df = pd.read_csv(file_url)
 df = df.head(100)
 
+# Приведение типов
+df["Year"] = df["Year"].astype(int)
+df["Country"] = df["Country"].astype(str)
+df["Feedstock_Yield"] = df["Feedstock_Yield"].astype(float)
+df["Production_Capacity"] = df["Production_Capacity"].astype(float)
+df["Processing_Tech_Efficiency"] = df["Processing_Tech_Efficiency"].astype(float)
+df["Energy_Consumption"] = df["Energy_Consumption"].astype(float)
+df["Feedstock_Cost"] = df["Feedstock_Cost"].astype(float)
+df["Transportation_Cost"] = df["Transportation_Cost"].astype(float)
+df["Distribution_Cost"] = df["Distribution_Cost"].astype(float)
+df["Carbon_Emissions"] = df["Carbon_Emissions"].astype(float)
+df["Water_Usage"] = df["Water_Usage"].astype(float)
+df["Market_Demand"] = df["Market_Demand"].astype(int)
+df["Price_Per_Gallon"] = df["Price_Per_Gallon"].astype(float)
+df["Govt_Incentive"] = df["Govt_Incentive"].astype(float)
+df["Bioethanol_Growth"] = df["Bioethanol_Growth"].astype(float)
+
+# Вывод
+print("\nТипы данных перед записью:")
+print(df.dtypes)
+
 # Создание SQLAlchemy движка
 engine = create_engine(
     f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
